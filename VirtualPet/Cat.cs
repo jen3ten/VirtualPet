567b9613e46929b367e0be7de84772e89fc163c4
mega24cde <megan.denman@gmail.com>
2019-05-17 14:25:07 -0400

    start/exit

--- VirtualPet/Cat.cs
+++ VirtualPet/Cat.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+/*As a player, I would like to add a pet to the shelter, so there is a pet that I can interact with
+Given I'm a player, when I choose to add a pet to the shelter from the main menu, then I am asked for its name and species
+*/
 namespace VirtualPet
 {
     class Cat
     {
+        public string Name;
+        public string Species;
+
+        Cat(string name, string species)
+        {
+            Name = name;
+            Species = species;
+                }
     }
 }
+    
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -6,7 +6,26 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Virtual Pet!");
+            Console.WriteLine("Type in start to begin game. At any time, type exit to end game.");
+            bool gameIsRunning = false;                                         //program is off.
+            string mainMenu = Console.ReadLine();
+            if(mainMenu == "start")
+            {
+                gameIsRunning = true;
+            }
+
+
+            string gameInput;
+            while(gameIsRunning)
+            {
+                Console.WriteLine("What kind of pet do you want to create?");
+                Console.WriteLine("For cat type \"cat\".");
+                gameInput = Console.ReadLine();
+                            
+                if (gameInput == "exit")
+                    gameIsRunning = false;
+            }
         }
     }
 }
