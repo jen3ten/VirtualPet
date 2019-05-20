67388a6c7f6b9d7d314bafc44b4fab9b493e1206
CLux <cplux7@gmail.com>
2019-05-20 15:15:47 -0400

    added line breaks to space out information on user side

--- VirtualPet/Menu.cs
+++ VirtualPet/Menu.cs
@@ -16,36 +16,40 @@ namespace VirtualPet
             Console.WriteLine("Type 1 to create a pet.");
             Console.WriteLine("Type 2 to show pet info.");
             Console.WriteLine("Type 3 to show pet status");
-            Console.WriteLine("Type 4 to exit");
+            Console.WriteLine("Type 4 to exit\n");
+            
             string menuInput = Console.ReadLine();
 
             if (menuInput == "1")
             {
-                Console.WriteLine("Enter your pet name:");
+                
+                Console.WriteLine("\nEnter your pet name:");
                 petName = Console.ReadLine();
 
+                Console.WriteLine("");
                 Console.WriteLine("Enter your pet species:");
                 petSpecies = Console.ReadLine();
 
                 firstPet = new Pet(petName, petSpecies, 0, 0, 100);
 
-                Console.WriteLine(firstPet.Name + " was added to the shelter!");
+                Console.WriteLine("");
+                Console.WriteLine(firstPet.Name + " was added to the shelter!\n");
 
                 return true;
 
             }
             else if (menuInput == "2" && firstPet != null)
             {
-                Console.WriteLine("Your pet name is " + firstPet.Name + " and your pet is a " + firstPet.Species + ".");
+                Console.WriteLine("\nYour pet name is " + firstPet.Name + " and your pet is a " + firstPet.Species + ".\n");
 
                 return true;
 
             }
             else if (menuInput == "3" && firstPet != null)
             {
-                Console.WriteLine(firstPet.Name + "'s hunger is " + firstPet.Hunger);
+                Console.WriteLine("\n" + firstPet.Name + "'s hunger is " + firstPet.Hunger);
                 Console.WriteLine(firstPet.Name + "'s boredom is " + firstPet.Boredom);
-                Console.WriteLine(firstPet.Name + "'s health is " + firstPet.Health);
+                Console.WriteLine(firstPet.Name + "'s health is " + firstPet.Health + "\n");
 
                 return true;
             }
